6e62078a8f7eb657dfb9e1797670fe7d5ec2a25e
Pooya Razavian <pooya72@gmail.com>
2021-03-27 16:43:30 +0000

    rename starter to main for both css and js

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,9 +8,6 @@
 //
 // Import Bootstrap's bundle (all of Bootstrap's JS + Popper.js dependency)
 
-import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
-
-
 // Option 2
 //
 // Import just what we need
@@ -18,5 +15,6 @@ import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 // If you're importing tooltips or popovers, be sure to include our Popper.js dependency
 // import "../../node_modules/popper.js/dist/popper.min.js";
 
-//import "../../node_modules/bootstrap/js/dist/util.js";
-//import "../../node_modules/bootstrap/js/dist/modal.js";
+import "../../node_modules/bootstrap/js/dist/util.js";
+import "../../node_modules/bootstrap/js/dist/modal.js";
+import "../../node_modules/bootstrap/js/dist/collapse.js";
